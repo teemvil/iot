4a18184e9cd81540e157012e3b5fede4363d80c8
Matias Vainio <matias.s.vainio@gmail.com>
2022-09-28 19:40:02 +0300

    simple route: GET api/images

--- flask/server/server.py
+++ flask/server/server.py
@@ -1,38 +1,12 @@
 from flask import Flask, request, jsonify
-from extract_metadata import extract_metadata
 import json
 import uuid
 
 app = Flask(__name__)
 
-# This is here for testing purposes only. Data received from
-# the client is going to be stored in the blockchain or similar.
-images = []
-
-
-@app.route('/api/images', methods=['GET', 'POST'])
-def handle_images() -> str:
-    if request.method == 'GET':
-        # This here should return the image metadata stored somewhere.
-        return jsonify(images)
-    elif request.method == 'POST':
-        received_image = request.files['media']
-        metadata = extract_metadata(received_image)
-        function_that_needs_to_be_moved_somewhere(metadata)
-
-        return json.dumps(metadata)
-
-
-@app.route('/api/images/<int:image_id>', methods=['GET'])
-def get_single_image(image_id):
-    print(image_id)
-    return f'image_id {image_id}'
-
-
-def function_that_needs_to_be_moved_somewhere(data):
-    data['id'] = str(uuid.uuid4())
-    images.append(data)
-
+@app.route('/api/images', methods=['GET'])
+def get_image():
+    return 'Get image'
 
 if __name__ == '__main__':
     app.run()
