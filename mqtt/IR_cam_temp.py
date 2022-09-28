b6f8c25e22ca704588bff94b81d0146c72bd7bfd
Matias Vainio <matias.s.vainio@gmail.com>
2022-09-28 16:34:44 +0300

    remove unnecessary code

--- mqtt/IR_cam_temp.py
+++ mqtt/IR_cam_temp.py
@@ -3,6 +3,7 @@ import busio
 import board
 import adafruit_amg88xx
 import paho.mqtt.client as mqtt
+import json
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
@@ -11,10 +12,6 @@ client = mqtt.Client()
 client.connect("192.168.11.79", 1883, 60)
 
 while True:
-    for row in amg.pixels:
-        print(['{0:.1f}'.format(temp) for temp in row])
-        print("")
-    print("temp: ", amg.temperature)
-    client.publish("image/temperature", payload=amg.temperature)
-    print("\n")
+    client.publish("sensor/temperature", payload=amg.temperature)
+    client.publish("pixel/temperature", payload=json.dumps(amg.pixels))
     time.sleep(2)
