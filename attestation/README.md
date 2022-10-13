f4528c95c6a1b585594da7dac8a96595c1120d44
Matias Vainio <matias.s.vainio@gmail.com>
2022-10-13 12:47:11 +0300

    attestation startup script added

---
+++ attestation/README.md
@@ -0,0 +1,7 @@
+# Notes
+Startup script runs when the device is turned on.
+
+Location of the script on the pi currently is `/usr/local/bin/startup_script.sh`
+
+And the service is called `startupCheck.service`. The file is located `/etc/systemd/system/startupCheck.service`
+
---
+++ attestation/startup_app.sh
@@ -0,0 +1,21 @@
+#!/bin/python3
+import paho.mqtt.client as mqtt
+import socket
+import json
+import requests
+
+client = mqtt.Client()
+client.connect("192.168.11.79", 1883, 60)
+
+hostname = socket.gethostname()
+url = f"http://192.168.11.79:8520/element/name/{hostname}"
+
+response = requests.get(url).json()
+id = response["itemid"]
+
+payload = {
+    "itemid": id,
+    "hostname": hostname
+}
+
+client.publish(f"alert/{hostname}", json.dumps(payload))
