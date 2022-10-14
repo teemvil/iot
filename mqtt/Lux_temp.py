9e9ac35f6cbf5a5611a78b4d415b86996c986923
Teemu <56117294+teemvil@users.noreply.github.com>
2022-10-14 14:26:43 +0300

    Update Lux_temp.py
    Added conditional status change Light vs Dark

--- mqtt/Lux_temp.py
+++ mqtt/Lux_temp.py
@@ -13,11 +13,33 @@ PORT=1883
 
 client = mqtt.Client()
 client.connect(IP, PORT, 60)
+status_old = bool(float(tsl.lux) > 46)
+change = True
+
+if float(tsl.lux) > 46:
+     client.publish("alert/iotp015/lux", payload="Status: Light")
+     print("Status: Light")
+else:
+     client.publish("alert/iotp015/lux", payload="Status: Dark")
+     print("Satus: Dark")
 
 while True:
-    print("sensor/lux: " + str(tsl.lux))
-    client.publish("data/iotpi015/sensor/lux", payload=tsl.lux)
-    if float(tsl.lux) > 45:
-         client.publish("alert/lux", payload="Alert, brightness is over 45")
-    #client.publish("pixel/temperature", payload=json.dumps(amg.pixels))
-    time.sleep(2)
+     print("sensor/lux: " + str(tsl.lux))
+     client.publish("data/iotpi015/sensor/lux", payload=tsl.lux)
+
+     status = bool(float(tsl.lux) > 46)
+     if status != status_old:
+          change = True
+     else:
+          change = False
+
+     if change:
+          if status:
+               client.publish("alert/iotp015/lux", payload="Status: Light")
+               print("Status: Light")
+          else:
+               client.publish("alert/iotp015/lux", payload="Status: Dark")
+               print("Satus: Dark")
+
+     status_old = status
+     time.sleep(2)
