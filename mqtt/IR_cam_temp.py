cf9c1562e85d49a1c265ca037eea6b4e3b5a9fb0
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-10-17 15:04:31 +0300

    IR sensor now sends mean value from the pixels. Not the most accurate, but might be usable.

--- mqtt/IR_cam_temp.py
+++ mqtt/IR_cam_temp.py
@@ -4,9 +4,11 @@ import board
 import adafruit_amg88xx
 import paho.mqtt.client as mqtt
 import json
+import math
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
+client.publish("management", payload="Iotpi014 IR sensor started")
 
 IP="192.168.11.79"
 PORT=1883
@@ -15,6 +17,16 @@ client = mqtt.Client()
 client.connect(IP, PORT, 60)
 
 while True:
-    client.publish("sensor/temperature", payload=amg.temperature)
-    client.publish("pixel/temperature", payload=json.dumps(amg.pixels))
+    #client.publish("sensor/temperature", payload=amg.temperature)
+    #client.publish("pixel/temperature", payload=json.dumps(amg.pixels))
+    
+    mean = 0
+    for column in amg.pixels:
+        mean = sum(column) / len(amg.pixels)
+        
+    print(mean)
+    client.publish("data/iotpi014/sensor/ir/pixels", payload=math.floor(mean))
+    
+    if mean > 31:
+        client.publish("alert", payload="Hot!")
     time.sleep(2)
