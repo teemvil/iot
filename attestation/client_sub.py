0949032a8555eeae967e4d10449b1e32a515afb5
Matias Vainio <matias.s.vainio@gmail.com>
2022-10-19 13:29:50 +0300

    Mqtt listener. used mainly for testing purposes

---
+++ attestation/client_sub.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+IP="192.168.11.79"
+PORT=1883
+
+# This is just a testing function. It listens to changes in the topic given to the
+# client.subscirbe() function.
+def on_connect(client, userdata, flags, rc):
+    print("connected with result code " + str(rc))
+
+    client.subscribe("alert/iotpi014")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(IP, PORT, 60)
+
+client.loop_forever()
