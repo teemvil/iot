f71cd7f7673581f28f770f482c11b037755c8fa7
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-10-21 17:14:18 +0300

    Created base class for sensors called Device. Created a test subclass called LuxSensor in the Lux_subclass.py file. Created a main.py file for testing the new classes easily

---
+++ sensors/Lux_subclass.py
@@ -0,0 +1,16 @@
+from device import Device
+
+class LuxSensor(Device):
+    
+    def __init(self):
+        super.__init__()
+    
+    
+    def send_message():
+        print("test")
+        
+    def measure_stuff(self):
+        print("measuring stuff")
+    
+    def start_up():
+        print("test")
---
+++ sensors/main.py
@@ -0,0 +1,30 @@
+from Lux_subclass import LuxSensor
+from device import Device
+
+# function to test if parameter base class is Device
+def base_test(sensor):
+    if sensor.__class__.__bases__[0] == Device:
+        print("Base class is Device")
+
+def main():
+    LuxSensor.send_message()
+    test = LuxSensor()
+    test.measure_stuff()
+    test2 = LuxSensor()
+    arr = [test, test2]
+    
+    testivar = "22"
+    print(testivar.__class__.__bases__[0])
+    
+    base_test(testivar)
+    base_test(test)
+    base_test(test2)
+    print(arr)
+    
+    #shows the base class
+    print(test.__class__.__bases__[0])
+    
+    print(Device)
+    
+if __name__ == "__main__":
+    main()
