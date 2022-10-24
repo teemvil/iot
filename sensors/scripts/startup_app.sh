12be210fd5b8c4054f3864394ea58046f6d42ab3
Matias Vainio <matias.s.vainio@gmail.com>
2022-10-24 14:06:40 +0300

    feature: start another process

--- sensors/scripts/startup_app.sh
+++ sensors/scripts/startup_app.sh
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import socket
 import json
 import requests
+import os
 
 client = mqtt.Client()
 client.connect("192.168.11.79", 1883, 60)
@@ -16,6 +17,11 @@ id = response["itemid"]
 payload = {
     "itemid": id,
     "hostname": hostname
+    "operation": "startup"
 }
 
 client.publish(f"management", json.dumps(payload))
+
+# Currently the path is just a test file
+# If everything is ok start the device manager here:
+os.system("python3 /home/metropolia/Innovationproject2022/controller/device_manager/test.py")
