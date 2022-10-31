55815a08d2a78416e1cb38dc6867a71fa62a8701
Matias Vainio <matias.s.vainio@gmail.com>
2022-10-31 14:07:20 +0200

    add temp payload

--- sensors/scripts/startup_app.py
+++ sensors/scripts/startup_app.py
@@ -1,36 +1,36 @@
-#!/bin/env python3
 import paho.mqtt.client as mqtt
 import socket
 import json
-import requests
-import threading
-import os
 
-def setup():
-    mgr = device_manager
-    thread = threading.Thread(target=mgr.run)
-    thread.start()
 
 def start():
     client = mqtt.Client()
     client.connect('192.168.11.79')
 
     payload = {
-        'hostname': socket.gethostname(),
-        'operation': 'process-startup',
-        'type': '',
-        'ip': '123.123.123.123',
-        'metadata': {
-
+        "itemid": "",
+        "hostname": "",
+        "ip": "",
+        "message": "",
+        "event": "",
+        "device": {
+            "valid": False,
+            "timestamp": ""
+        },
+        "sensor": {
+            "name": "",
+            "timestamp": ""
+        },
+        "timestamp": "",
+        "client": {
+            "host": "192.168.11.79",
+            "port": 1883,
+            "keepalive": 60
         }
     }
 
     client.publish('management', payload=json.dumps(payload))
 
-    payload.update({'type': 'attest'})
-    client.publish('management', payload=json.dumps(payload))
-
 
 if __name__ == '__main__':
-    setup()
     start()
