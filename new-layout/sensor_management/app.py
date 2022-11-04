4c7735d00deb97d27056f1f85cf6c86ee41f19ce
Matias Vainio <matias.s.vainio@gmail.com>
2022-11-04 11:30:17 +0200

    import json, concretesensor

--- new-layout/sensor_management/app.py
+++ new-layout/sensor_management/app.py
@@ -1,8 +1,11 @@
 from multiprocessing.dummy import Manager
+
+from device.sensors.ConcreteSensor import ConcreteSensor
 from device.Device import Device
 from device.sensors.Sensor import Sensor
 from device.Manager import Manager
 import threading
+import json
 
 
 def start():
