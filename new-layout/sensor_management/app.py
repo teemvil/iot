9f1ef2af0927ca0d48eab0768d01c0c42b715cb8
Matias Vainio <matias.s.vainio@gmail.com>
2022-11-14 14:22:31 +0200

    moving things to new locations

--- new-layout/sensor_management/app.py
+++ new-layout/sensor_management/app.py
@@ -1,21 +1,11 @@
-from multiprocessing.dummy import Manager
-
-from device.sensors.ConcreteSensor import ConcreteSensor
-from device.Device import Device
-from device.sensors.Sensor import Sensor
-from device.Manager import Manager
+from device.sensors.TofSensor import ToFSensor
 import threading
 import json
 
 
 def start():
-    m = Manager()
-    s = Sensor()
-
-    t1 = threading.Thread(target=m.run)
-    t2 = threading.Thread(target=s.start_up)
-    t1.start()
-    t2.start()
+    s = ToFSensor(1, "tof")
+    s.measure_stuff()
 
 
 if __name__ == '__main__':
