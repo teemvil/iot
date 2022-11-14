b6ffdae884cfc937f76de539b422cd15896dc093
Matias Vainio <matias.s.vainio@gmail.com>
2022-11-14 15:57:19 +0200

    change to open config from project directory

--- sensor_management/device/IoTElement.py
+++ sensor_management/device/IoTElement.py
@@ -1,19 +1,23 @@
 import paho.mqtt.client as mqtt
 import json
+import os
 
 
 class IoTElement:
 
     def __init__(self):
         self.client = mqtt.Client()
-        self.config = self.__read_config()
+        self.config = self.read_config()
         self.ip = self.config["client"]["host"]
         self.port = self.config["client"]["port"]
         self.client.connect(self.ip, self.port, 60)
 
-    def __read_config(self):
-        with open('/etc/iotDevice/data_packet.json', 'r') as f:
-            return json.loads(f.read())
+    def read_config(self):
+        here = os.path.dirname(os.path.abspath(__file__))
+        file_name = os.path.join(here, 'config.json')
+
+        with open(file_name, 'r') as f:
+            return json.load(f)
 
     def run(self):
         print("IoTElement RUN")
