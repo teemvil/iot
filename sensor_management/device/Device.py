92bf48d36c89b29d932c499e8dde16d94ee24032
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-11-20 23:12:47 +0200

    Made the device listen to management channel and send a startup message. Changed the read_config method to accept a filename parameter.

--- sensor_management/device/Device.py
+++ sensor_management/device/Device.py
@@ -1,15 +1,39 @@
-from IoTElement import IoTElement
+from .IoTElement import IoTElement
 import json
 import time
 
-# Is this thing here needed?
-# Change inheritance.
 class Device(IoTElement):
     def __init__(self):
         super().__init__()
+        
+        self.device_config = self.read_config("device.json")
+        self.itemid = self.device_config["itemid"]
+        self.hostname = self.device_config["hostname"]
+        self.event = self.device_config["event"]
+        self.message = self.device_config["message"]
+        self.address = self.device_config["address"]
+        self.timestamp = self.device_config["timestamp"]
+        
+        self.startup()
+        
+        try:
+            while True:
+                #self.client.publish("management", json.dumps({'message': 'doing stuff'}))
+                print("doing stuff")
+                time.sleep(1)
+        except:
+            print("Exception during while loop, exiting...")
+            self.client.loop_stop()
+            self.client.disconnect()
+                
 
-    def startup_message(self):
-        self.connect()
-        while True:
-            self.client.publish('test', json.dumps({'message': 'startup'}))
-            time.sleep(2)
+    def startup(self):
+        self.client.subscribe("management")
+        self.client.on_message = self.on_message
+        self.client.loop_start()
+        self.client.publish('management', json.dumps({"itemid":self.itemid, "address": self.address, "hostname": self.hostname, "event": self.event , 'message': self.message, "timestamp": self.timestamp}))
+        
+    def on_message(self, client, nonetype, msg):
+        print(f"Received message with topic: '{msg.topic}' and message: '{msg.payload.decode()}'")
+    
+    
