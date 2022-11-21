73d0b5c358a006e089637b17b0f55edf9434eb98
Jani Peltonen <peltonen.jani@hotmail.fi>
2022-11-21 12:29:46 +0200

    minor changes to Device

--- sensor_management/device/Device.py
+++ sensor_management/device/Device.py
@@ -9,10 +9,7 @@ class Device(IoTElement):
         self.device_config = self.read_config("device.json")
         self.itemid = self.device_config["itemid"]
         self.hostname = self.device_config["hostname"]
-        self.event = self.device_config["event"]
-        self.message = self.device_config["message"]
         self.address = self.device_config["address"]
-        self.timestamp = self.device_config["timestamp"]
         
         self.startup()
         
@@ -31,7 +28,7 @@ class Device(IoTElement):
         self.client.subscribe("management")
         self.client.on_message = self.on_message
         self.client.loop_start()
-        self.client.publish('management', json.dumps({"itemid":self.itemid, "address": self.address, "hostname": self.hostname, "event": self.event , 'message': self.message, "timestamp": self.timestamp}))
+        self.client.publish('management', json.dumps({"itemid":self.itemid, "address": self.address, "hostname": self.hostname, "event": "startup" , 'message': "Hello world", "timestamp": time.localtime()}))
         
     def on_message(self, client, nonetype, msg):
         print(f"Received message with topic: '{msg.topic}' and message: '{msg.payload.decode()}'")
