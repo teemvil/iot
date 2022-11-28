75936df7532e2bd3254488c328c49291daa31c2f
Matias Vainio <matias.s.vainio@gmail.com>
2022-11-28 11:24:43 +0200

    changes added

--- IoT/IoTLibrary/Manager.py
+++ IoT/IoTLibrary/Manager.py
@@ -1,6 +1,6 @@
-from device.IoTElement import IoTElement
+from IoTElement import IoTElement
 from datetime import datetime
-import controller.attest as att
+import attest as att
 import json
 import threading
 
@@ -9,18 +9,11 @@ class Manager(IoTElement):
     def __init__(self):
         super().__init__()
 
-    def __attest_validate(self, json_update):
-        # Attest validate needs to be determined how we do it
-        json_object = json_update
-        json_object["event"] = "validating device"
-        json_object["message"] = "Device validation in process"
-        self.client.publish(f"management/verify", json.dumps(json_object))
-
     def __on_connect(self, client, userdata, flags, rc):
         print("devmanager connected with result code " + str(rc))
         json_object = self.message
-        json_object["message"] = "Sensor manager running"
-        json_object["event"] = "starting sensor manager"
+        json_object["message"] = "Manager running"
+        json_object["event"] = "manager startup"
         json_object["timestamp"] = self.__get_time_stamp()
         self.client.publish("management", json.dumps(json_object))
         # print(json_object)
@@ -29,24 +22,20 @@ class Manager(IoTElement):
         decoded_message = str(msg.payload.decode("utf-8"))
         json_object = json.loads(decoded_message)
         # print(json_object)
+        if (json_object["event"] == "device startup"):
+            valid_object = att.check_validity(json_object)
+            if (valid_object["event"] == "device valdation ok"):
+                self.__publish_datat(valid_object)
+            else:
+                json_object["event"] = "device validation fail"
+                json_object["message"] = "Validation unsuccesfull"
+                json_object["device"]["validtimestamp"] = self.__get_time_stamp()
+                json_object["messagetimestamp"] = self.__get_time_stamp()
+                self.__publish_datat(json_object)
 
-        if json_object["device"]["hostname"] == "iotpi012":
-            print("iotpi12 functionality")
-            # Here comes the specific functions for validation and all other necessary things like reset etc.
-
-        if json_object["device"]["hostname"] == "iotpi014":
-            print("iotpi14 functionality")
-            # Here comes the specific functions for validation and all other necessary things like reset etc.
-
-        if json_object["device"]["hostname"] == "iotpi015":
-            print("iotpi15 functionality")
-            # Here comes the specific functions for validation and all other necessary things like reset etc.
-
-        if json_object["device"]["hostname"] == "iotpi016":
-            print("iotpi16 functionality")
-            obj = att.check_validity(json_object)
-            print(obj)
-            # Here comes the specific functions for validation and all other necessary things like reset etc.
+        if (json_object["event"] == "sensor startup"):
+            # att.check_sensor_validity(json_object)
+            print("Sensor validation")
 
     def __get_time_stamp(self):
         now = datetime.now()
@@ -62,7 +51,6 @@ class Manager(IoTElement):
         self.client.subscribe('management')
         self.client.on_connect = self.__on_connect
         self.client.on_message = self.on_message
-
         self.client.loop_forever()
 
     def __publish_datat(self, json_update):
